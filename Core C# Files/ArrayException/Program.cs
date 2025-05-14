c1490184e4e9e5fd0b8afffd6c421fe54a7d256c
Nisanth-Saravanan <nisanth.saravanan@relevantz.com>
2025-05-14 10:25:17 +0530

    Exceptions Commit.

---
+++ Core C# Files/ArrayException/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ArrayException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            try
+            {
+                arr[5] = 6;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Array index is out of range!");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed!");
+            }
+        }
+    }
+}
