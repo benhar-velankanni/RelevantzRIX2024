05c06c2f71e83978c5fbabb7081ebfd0f67714c4
Nisanth-Saravanan <nisanth.saravanan@relevantz.com>
2025-05-14 09:47:37 +0530

    Image Interface Commit.

---
+++ Core C# Files/IImage/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace IImage
+{
+    interface IPict
+    {
+        void deleteImage();
+        void showImage();
+    }
+
+    interface IPictManPic
+    {
+        void applyAlpha();
+        public void showImage();
+    }
+
+    class MyImage : IPict, IPictManPic
+    {
+        public void deleteImage()
+        {
+            Console.WriteLine("Image deleted.");
+        }
+        void IPict.showImage()
+        {
+            Console.WriteLine("Image shown.");
+        }
+        public void applyAlpha()
+        {
+            Console.WriteLine("Alpha applied.");
+        }
+        void IPictManPic.showImage()
+        {
+            Console.WriteLine("Image shown in another way.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyImage myImage = new MyImage();
+            myImage.deleteImage();
+            myImage.applyAlpha();
+            ((IPict)myImage).showImage();
+            ((IPictManPic)myImage).showImage();
+
+        }
+    }
+}
