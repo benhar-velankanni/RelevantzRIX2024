7aa3c6316ab73af576fb804769ee84158d17ec50
Nisanth-Saravanan <nisanth.saravanan@relevantz.com>
2025-05-15 09:52:04 +0530

    Anonymous Method Example Commit.

---
+++ Core C# Files/AnonMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    delegate int MathOperation(int x, int y);
+
+    static void Main()
+    {
+        MathOperation add = delegate (int x, int y)
+        {
+            Console.WriteLine("Addition:");
+            return x + y;
+        };
+        MathOperation subtract = (x, y) => x - y;
+
+        Console.WriteLine(add(10, 5)); // Outputs 15
+        Console.WriteLine("Subtraction:");
+        Console.WriteLine(subtract(10, 5)); // Outputs 5
+    }
+}
