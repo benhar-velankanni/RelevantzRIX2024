706ee88afecb9a7409e73bdeba043f0ff2184610
Nisanth-Saravanan <nisanth.saravanan@relevantz.com>
2025-05-15 12:38:44 +0530

    Threads Example Commit.

---
+++ Core C# Files/Lamda/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UsingLamdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetEvenNumbers.EvenNumbers();
+        }
+    }
+
+    public class GetEvenNumbers
+    {
+        public static void EvenNumbers()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine($"Data: {num}");
+            }
+        }
+    }
+}
+
---
+++ Core C# Files/ThreadsExample/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ThreadMultithreadApp
+{
+    public class UsingTread
+    {
+        public static void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 1: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+        public static void Function2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 2: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(UsingTread.Function1);
+            Thread t2 = new Thread(UsingTread.Function2);
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
