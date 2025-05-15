aac9e903e9620e96bc4b0a1db14913e307941a00
Nisanth-Saravanan <nisanth.saravanan@relevantz.com>
2025-05-15 11:39:29 +0530

    Generics Example Commit.

---
+++ Core C# Files/GenericsExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using GenricApplication;
+
+namespace GenricApplication
+{
+
+    public class UsingGeneric<T>
+    {
+        private T a;
+        private T b;
+
+        public UsingGeneric(T x, T y)
+        {
+            a = x;
+            b = y;
+        }
+
+        public bool CompareResult()
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+class Program
+{
+
+    public static void Main()
+    {
+
+
+        UsingGeneric<string> stringGenvalue = new UsingGeneric<string>("Nisanth", "Saravanan");
+        UsingGeneric<int> intGenintvalue = new UsingGeneric<int>(100, 100);
+
+        Console.WriteLine(stringGenvalue.CompareResult());
+        Console.WriteLine(intGenintvalue.CompareResult());
+    }
+}
